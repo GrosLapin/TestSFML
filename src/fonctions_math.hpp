e212d5ea62fb4603a95bad6d46d6b86d436b3244
MartinMorterol <martin.morterol@gmail.com>
2016-06-08 13:43:28 +0200

    suppression d'un warning de conversion

--- src/fonctions_math.hpp
+++ src/fonctions_math.hpp
@@ -5,7 +5,10 @@
 
 #include <boost/math/distributions/normal.hpp> // for normal_distribution
 
+#include "fonctions_grapiphiques.hpp"
 #include "traits.hpp"
+#include <type_traits>
+
 
 namespace testSFML {
 
@@ -24,10 +27,21 @@ namespace testSFML {
     {
         /// DANGER : COMMENT je fais ça proprement cast du int en float ? ou en double enfin bref comment je cast
         /// en "le plus precis"
+
+		// Je ne sais pas si c'est ce que je veux faire, globalement je dis au compilo, "je sais ce que je fais"
+		// mais je sais pas si je devrait pas laisser le warning, en me disant que si le mec a mis -WConversion 
+		// c'est qu'il veut l'avoir ?
+		using typeRetour = typename std::common_type<decltype(getX(p1)),
+											decltype(getX(p2)),
+											decltype(getY(p1)),
+											decltype(getY(p2))
+										   >::type ;
+		
+		
         return sqrt(
-                        pow (getX(p1) -getX(p2), 2)
+                        pow (typeRetour( getX(p1) ) - typeRetour( getX(p2) ), 2)
                         +
-                        pow (getY(p1) -getY(p2), 2)
+                        pow (typeRetour( getY(p1) ) - typeRetour( getY(p2) ), 2)
                     );
     }
 
