bea8c93cbf4c07d1478f5d99bb6ebda8cb87cb0e
Your Name <you@example.com>
2016-06-12 19:21:53 +0200

    oublie d'un fichier

---
+++ src/convertion.hpp
@@ -0,0 +1,51 @@
+#ifndef CONVERSION_HPP
+#define CONVERSION_HPP
+
+
+#include "traits.hpp"
+
+namespace testSFML {
+
+    template <class T>
+    T convert_to(const std::string& str) {
+        static_assert(std::is_same<T,T>::value && false, "Conversion vers un type inconnu.");
+        return T();
+    }
+
+    template <>
+    double convert_to<double> (const std::string& str) {
+        return std::atof(str.c_str());
+    }
+
+    template <>
+    int convert_to<int> (const std::string& str) {
+        return std::atoi(str.c_str());
+    }
+
+    template <>
+    std::string convert_to<std::string> (const std::string& str) {
+        return str;
+    }
+        
+        
+        /***
+	 *
+	 *
+	 * Fait la conversion d'un point à un autre. globalement ça sert a dire au compilateur qu'on sais ce qu'on fait
+	 * Et qu'on veux pas de warnings
+	 *
+	 ***/
+        template <  class PointCible,
+                    class PointFrom,
+                    class X = typename std::enable_if< is_point<PointCible>::value >::type,
+                    class W = typename std::enable_if< is_point<PointFrom>::value >::type>
+	PointCible convert_to (const PointFrom && from)
+	{
+			using type_cible_x = decltype ( getX ( std::declval<PointCible>() ));
+			using type_cible_y = decltype ( getY ( std::declval<PointCible>() ));
+			return { type_cible_x(getX(from)) , type_cible_y(getY(from)) };
+	}
+}
+
+
+#endif
