9109484efe94738cf39b0942bd6a6349d61c7902
Your Name <you@example.com>
2016-10-31 18:55:54 +0100

    avancement de la refactorisaiton

--- src/outils/gestion_option.hpp
+++ src/outils/gestion_option.hpp
@@ -30,24 +30,6 @@ class gestion_option
 
     private :
 
-        struct IdValeur {
-            std::string id;
-            std::string valeur;
-
-            inline bool operator == (const IdValeur& other) const
-            {
-                return id == other.id;
-            }
-
-            inline bool operator == (const std::string & o_id) const
-            {
-                return o_id == id;
-            }
-            
-            inline bool has_no_value () const { return valeur.empty(); } 
-            //IdValeur (std::string unId, std::string uneValeur) : id(unId),valeur(uneValeur) {}
-        };
-
         const std::string separateur = "=";
         const std::string commentaire= "#";
 
@@ -73,15 +55,36 @@ class gestion_option
         
         std::string message_aide,message_fichier;
 
-
-        std::vector<IdValeur> vec_args_named;
-        std::vector<std::string> vec_args_raw;
-
-        std::vector<IdValeur> vec_option_default, vec_option_fichier;
-        std::vector<std::string> vec_option;
+        
+        // la version refactor
+        // Cette version prends pas en compte les options qui n'ont pas de valeur (erreur ?)
+        // et leur donne la valeur "", a fixer si besoin
+        struct value_ : public std::string
+        {
+          std::string value;
+          bool is_set = true;
+          value_ () = default; // je suis pas bien sur de savoir pourquoi j'ai besoin de ça, mais clairement j'en ai beosin
+          value_ (std::string v, bool ok = true ) : value(v), is_set(ok) {}
+        };
+        
+        std::map<std::string,value_> options;
+        std::vector<std::string> args_raw;
+        
+        bool add_option_if_empty (std::string id, std::string value, bool set = true)
+        {
+            if (  contain_key(options,id) && ! options[id].is_set )
+            {
+                options[id] = value_(value,set);
+            }
+            if (  ! contain_key(options,id) )
+            {
+                options[id] = value_(value,set);
+            }
+            return true;
+        }
 
         /// TODO : le fair pour autre chose que des strings
-        std::map < std::string , std::vector < std::string >> map_option_valide_value;
+        //std::map < std::string , std::vector < std::string >> map_option_valide_value;
 
         bool hasSeparateur (std::string param)
         {
@@ -108,7 +111,7 @@ class gestion_option
 
         //fonction qui empeche de croire qu'on a mis un param alors qu'on a fait une faute de frappe :D
         inline void check_typo() const {
-            
+            /*
             // check des option 
             for (auto opt : vec_args_named ) {
 
@@ -130,20 +133,22 @@ class gestion_option
                    erreur ("L'option \"" + opt.id + "\" n'est pas prévu par le programme.");
                }
             }
+            */
         }
         
         // pour les options ayant une list de valeur, on verifi qu'on est bon 
-        inline void check_liste_value (const std::string & id , const std::string & value) const
+        /*inline void check_liste_value (const std::string & id , const std::string & value) const
         {
             if ( contain_key ( map_option_valide_value , id)  and not contain ( map_option_valide_value.at(id) , value ) )
             {
                 erreur ("L'option \"" + id + "\" n'est pas dans les valeurs possible : " + to_string (map_option_valide_value.at(id)));
             }
-        }
+        }*/
     
         // un peut comme help on peut taper --generate pour generer des fichiers de param 
         // avec les valeurs par defaut
         inline void generate () const {
+            
             if ( generate_file ) {
                 std::ofstream flux (nomProgramme+".param");
                 auto make_commentaire = [&](const std::string & ligne) 
@@ -152,18 +157,23 @@ class gestion_option
                 };
                 make_commentaire ("fichier auto generé");
                 make_commentaire (" vous pouvez l'utiliser en ajoutant l'option suivante a votre ligne de commande");
-                make_commentaire (" --load-file"+separateur+"nomProgramme+".param");
+                make_commentaire (" --load-file"+separateur+nomProgramme+".param");
                 make_commentaire ("");
-                make_commentaire ("Option par defaut du programme :");
-                for (const auto& pair : vec_option_default )
+                make_commentaire ("Option ayant deja une valeur par defaut ");
+                for (const auto& pair : options)
                 {
-                    flux << pair.first << separateur <<pair.second << std::endl;
+                    if ( pair.second.is_set)
+                    flux << pair.first << separateur <<pair.second.value << std::endl;
                 }
                 make_commentaire ("Option sans valeur par defaut ( à décomenter et compléter ):");
-                for (const auto& s : vec_option )
+                
+                for (const auto& pair : options)
                 {
-                    make_commentaire(s+separateur);
+                    if ( ! pair.second.is_set)
+                    make_commentaire(pair.first+separateur);
+                    
                 }
+               
                 std::cerr << "Generation du fichier " << nomProgramme+".param" << std::endl;
                 exit(5);
             }
@@ -174,7 +184,7 @@ class gestion_option
         
         inline std::string get_separateur () const { return separateur; }
 
-        gestion_option (int argc, char** argv, std::string sep="=") : separateur(sep), vec_args_raw(), vec_option_default (), vec_option()
+        gestion_option (int argc, char** argv, std::string sep="=") : separateur(sep)
         {
             if ( argc < 1 ) {
                 std::cerr << "Un programme doit avoir au moins un param : son nom. Je sais pas ce que vous avez fait, mais faites pas ca ." << std::endl;
@@ -190,6 +200,9 @@ class gestion_option
             nbArg = (size_t)argc-1; // on peut faire la convertion grace au if du dessus
             nomProgramme = argv[0];
 
+            // on garde les fichiers dans l'ordre, pour les traiter apres, 
+            std::vector<std::string> fichiers;
+            
             // Pour toutes les options
             for (int i = 1 ; i < argc ; i ++ )
             {
@@ -202,17 +215,22 @@ class gestion_option
                     auto option_temps = decoupe(param,separateur);
                     if (option_temps[0]== "--load-file") 
                     {
-                        load_file(option_temps[1],true); // si le fichier n'existe pas on prévient l'user 
+                        fichiers.push_back(option_temps[1]);
                     }
                     else
                     {
-                        vec_args_named.push_back({option_temps[0],option_temps[1]});
+                        options[option_temps[0]] = option_temps[1];
                     }
                 }
                 else // si c'est un param brute
                 {
-                    vec_args_raw.push_back(param);
+                    args_raw.push_back(param);
                 }
+            }
+            
+            for (const auto& fichier : fichiers)
+            {
+               load_file(fichier,true); // si le fichier n'existe pas on prévient l'user    
             }
 
         };
@@ -231,7 +249,17 @@ class gestion_option
                     if ( hasSeparateur ( ligne) )
                     {
                         auto option_temps = decoupe(ligne,separateur);
-                        vec_option_fichier.push_back({option_temps[0],option_temps[1]});
+                        
+                        if (option_temps[0]== "--load-file") 
+                        {
+                            // on peut faire des includes dans les fichiers, 
+                            // danger avec les priorités (mais c'est pas mon probleme)
+                            load_file(option_temps[1],true);
+                        }
+                        else
+                        {
+                            add_option_if_empty(option_temps[0], option_temps[1] );
+                        }
                     }
                 }
                 
@@ -253,18 +281,16 @@ class gestion_option
 
         inline void min_raw_args(size_t min) const 
         {
-
-            if ( vec_args_raw.size() < min )
+            if ( args_raw.size() < min )
             {
                 erreur ("Erreur : Le programme prend au moins " + std::to_string(min) + " parametres sans noms" );
             }
-
         }
 
         inline void nb_raw_args_equal(size_t val) const { nb_raw_args_between(val,val); }
 
         inline void nb_raw_args_between (size_t min, size_t max) const {
-            if ( nbArg < min || nbArg > max ) {
+            if ( args_raw.size() < min || args_raw.size() > max ) {
                 if ( min == max ){
                      erreur ("Erreur : Le programme prend exactement " + std::to_string(min) + " parametres sans noms" );
                 }
@@ -279,15 +305,15 @@ class gestion_option
         inline T get_raw_val(size_t id){
             check_help();
             generate ();
-            if ( id < vec_args_raw.size() ){
-                erreur("vous avez demandez l'indice " + std::to_string(id) + " alors qu'il n'y a que " + std::to_string(vec_args_raw.size()) + " params");
+            if ( id >= args_raw.size() ){
+                erreur("vous avez demandez l'indice " + std::to_string(id) + " alors qu'il n'y a que " + std::to_string(args_raw.size()) + " params");
             }
-            return convert_to<T>(vec_args_raw[id]);
+            return convert_to<T>(args_raw[id]);
         }
 
         inline void allow_raw_args(bool allow)
         {
-            if (not allow and vec_args_raw.size() > 0 )
+            if (not allow and args_raw.size() > 0 )
             {
                 erreur("Le programme ne prend pas de parametre libre" );
             }
@@ -296,10 +322,10 @@ class gestion_option
         inline std::vector<std::string> get_raw_args (){
             check_help();
             generate ();
-            return vec_args_raw;
+            return args_raw;
         }
 
-        inline void mustBeValideFile(const std::vector<std::string>& listTag) const
+        inline void must_be_valide_file(const std::vector<std::string>& listTag) const
         {
             bool ok = true;
             std::string fichiers_fails = "";
@@ -325,21 +351,25 @@ class gestion_option
         inline void add(const std::string& option, const T& defaultVal)
         {
 			check_can_add ();
-            vec_option_default.push_back({option,std::to_string(defaultVal)});
+            add_option_if_empty(option,std::to_string(defaultVal));
         }
         
         inline void add(const std::string& option, const std::string& defaultVal)
         {
 			check_can_add ();
-            vec_option_default.push_back({option,defaultVal});
+            std::cout << "add" << std::endl;
+            add_option_if_empty(option,defaultVal);
         }
         
+
         inline void add(const std::string& option)
         {
 			check_can_add ();
-            vec_option.push_back(option);
+            add_option_if_empty(option,"",false);
         }
         
+        /*
+        // TODO : refactor ça avec un truc plus générique
         template<   class Conteneur = std::vector<std::string>,
                     class = std::enable_if_t<is_container<Conteneur>::value> 
                  >
@@ -361,6 +391,7 @@ class gestion_option
             vec_option_default.push_back({option,value});
             map_option_valide_value[option] = {  conteneur.begin(),conteneur.end()   };
         }
+        */
 
         
         // permet d'avoir une valeur avant la fin de la lecture des params
@@ -369,42 +400,16 @@ class gestion_option
 			check_help();
             generate ();
 
-            // est ce qu'on a l'option dans les param ? 
-            auto it = std::find(vec_args_named.begin(), vec_args_named.end(), id);
-            if ( it != vec_args_named.end() ) 
+            if ( ! contain_key (options,id) )
             {
-                
-                // on vérifi que si c'etait une value a choix multiple on est bien dans un choix valide
-                check_liste_value(id,it->valeur);
-                // si oui il a la priorité 
-                return convert_to<T>(it->valeur);
+                erreur("Le programme demande l'option "+ id +" qui n'est pas dans les options prévue par le programme... Dev tu t'es chier..");
             }
-            
-            // si non, est ce qu'il est dans le fichier ? 
-            auto it_fichier = std::find_if(vec_option_fichier.begin(), vec_option_fichier.end() , [&](auto paire) { return paire.first == id ;} );
-            if ( it_fichier!= vec_option_fichier.end() ) {
-                // on vérifi que si c'etait une value a choix multiple on est bien dans un choix valide
-                check_liste_value(id,it_fichier->second);
-                return convert_to<T>(it_fichier->second);
+            if ( options.at(id).is_set == false )
+            {
+                erreur("Le programme demande l'option "+ id +" qui n'a pas d'option par defaut, ni de valeur assignée");
             }
-            
-            // si toujours pas on regarde les options par defaut
-            auto if_defaut = std::find_if(vec_option_default.begin(), vec_option_default.end() , [&](auto paire) { return paire.first == id ;} );
-            // si on trouve pas on affiche une erreur
-            if ( if_defaut == vec_option_default.end() ) {
-                // on affiche le message d'erreur qui va bien
-                if (std::find(vec_option.begin(),vec_option.end(),id) != vec_option.end()) {
-                    erreur("L'option " +id+" n'a pas de valeur par defaut est n'est pas présente");
-                }
-                else{
-                    erreur("Le programme demande une option qui n'est pas dans les options prévue par le programme... Dev tu t'es chier..");
-                }
-            }
-            
-            // si on arrive la c'est qu'on a trouvé donc on est safe,
-            // j'ai du inverser la logique pour que le compilateur ne stress pas de ne pas avoir de retour;
-            check_liste_value(id,if_defaut->second);
-            return convert_to<T>(if_defaut->second);
+            return convert_to<T>(options.at(id).value);
+            // il faudra check les valeurs des opitons ayant un champs quand ça sera la 
 		}
         
         template<class T = std::string>
@@ -415,17 +420,14 @@ class gestion_option
             
         }
 
+        
         inline std::string usage() const {
             std::string retour =  "Usage : \n./"+get_name_file(nomProgramme)+" ";
-            for (auto& opt : vec_option)
+            for (const auto& opt : options)
             {
-                retour += opt+"=valeur ";
+                retour += opt.first+separateur+"valeur (default:"+opt.second.value+") ";
             }
-
-            for (auto& opt : vec_option_default)
-            {
-                retour += opt.first+"=valeur (default:"+opt.second+") ";
-            }
+            
             retour += "\n";
             retour +=  message_fichier;
             retour +=  message_aide;
